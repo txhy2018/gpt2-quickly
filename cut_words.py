aafed0f24316d49568883d0de57ccc94b591783f
小知 <46769235+txhy2018@users.noreply.github.com>
2023-05-19 13:24:26 +0800

    Update cut_words.py

--- cut_words.py
+++ cut_words.py
@@ -13,8 +13,8 @@ def cut_words(processer_num, text, result_dict):
         texts = text.split('\n')
         for line in tqdm(texts):
             try:
-                cuts = " ".join(jieba.cut(line))
-                out_f.write(cuts+'\n')
+                cuts = " ".join(jieba.cut(line))  # jieba分割文本，分割后以空格隔开
+                out_f.write(cuts+'\n')  # 保存分割后文本，以\n隔开
             except UnicodeDecodeError:
                 pass
             except KeyError:
@@ -27,7 +27,7 @@ def multiply_cut(handler, tasks):
     manager = Manager()
     result_dict = manager.dict()  # didn't work and don't know why
     jobs = []
-    for processer_num, task in enumerate(tasks):
+    for processer_num, task in enumerate(tasks):  # 多进程任务
         p = Process(target=handler, args=(
             processer_num, task, result_dict))
         jobs.append(p)
@@ -53,7 +53,7 @@ def split_data(
     text_task = []
     num_pre_task = len(text) // n_processes
     for i in range(0, len(text), num_pre_task):
-        text_task.append(text[i: i + num_pre_task])
+        text_task.append(text[i: i + num_pre_task])  # 将文本分为 num_pre_task 段文本
     return text_task
 
 
@@ -61,22 +61,22 @@ def split_data(
 @click.option('--n_processes', default=1, help='Number of processes.')
 def preprocess(n_processes):
     print(f'reading {configs.data.raw}')
-    with open(configs.data.raw, 'r') as f:
+    with open(configs.data.raw, 'r') as f:  # 1、读取原始文本数据集
         data = f.read().replace('  ', ' ').replace('\n\n', '\n')
         print(f"total words: {len(data)}")
 
     print(f"split data into {n_processes} pieces")
-    text_task = split_data(data, n_processes)
+    text_task = split_data(data, n_processes)  # 2、将一个文本字符串分为几段
 
-    multiply_cut(cut_words, text_task)
+    multiply_cut(cut_words, text_task)  # 3、将分段文本进行多进程jieba分割，并分别保存到raw.cut.temp.{processer_num}.txt文件，加快速度
 
     path = Path(configs.data.path)
-    with open(configs.data.raw_cut, 'w') as all_cut_file:
-        for filename in path.glob('raw.cut.temp.*'):
+    with open(configs.data.raw_cut, 'w') as all_cut_file:  # 4、汇总合并多进程保存的分割文本文件，以\n换行符隔开
+        for filename in path.glob('raw.cut.temp.*'):  # 读取多进程分割后的文件
             with open(filename) as cut_file:
                 all_cut_file.write(cut_file.read()+'\n')
                 print(f'dropping {filename}')
-                os.system(f'rm {filename}')
+                os.system(f'rm {filename}')  # 删除多进程分割后的文件
 
 
 if __name__ == '__main__':
